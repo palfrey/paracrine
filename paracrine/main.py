c25afb1116592b5691ef61ac9949f8406ae64167
Tom Parker-Shemilt <palfrey@tevp.net>
2023-01-20 15:19:32 +0000

    Untested pleroma test

--- paracrine/main.py
+++ paracrine/main.py
@@ -1,36 +1,28 @@
 import logging
-import sys
-from typing import Any, Dict
+from typing import Any, Callable, Dict, Optional
 
-from paracrine.core import main
+from .core import main, run
 
-from .config import set_data
+from .config import set_config, set_data
 from . import bootstrap
 
 from mitogen.parent import Router
 
 
-def do(data: Dict[str, Any]) -> None:
+def do(data: Dict[str, Any], core_func: Callable) -> None:
     set_data(data)
+    core_func()
 
-    blogs.do()
-    pleroma.do()
-
-
-def core(router: Router) -> None:
-    for info in main(router, do):
+def core(router: Router, core_func: Callable) -> None:
+    for _ in main(router, do, core_func):
         pass
 
-def everything(config: str):
+def everything(inventory_path: str, bootstrap_func: Optional[Callable], core_func: Callable):
     logging.basicConfig()
     logging.root.setLevel(logging.INFO)
 
-    set_config(config)
-    config = get_config()
-
+    set_config(inventory_path)
     run(bootstrap.core)
-    run(core)
-
-
-if __name__ == "__main__":
-    everything(sys.argv[1])
+    if bootstrap_func is not None:
+        run(bootstrap_func())
+    run(core, core_func)
