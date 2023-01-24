e1faa61af14822adc0bce5bb2128fc331210e08a
Tom Parker-Shemilt <palfrey@tevp.net>
2023-01-24 22:40:18 +0000

    Remove strict host key checking from login

--- paracrine/login.py
+++ paracrine/login.py
@@ -7,7 +7,9 @@ from .config import get_config, path_to_config_file, set_config
 def ssh_server(server, run=""):
     command = f"ssh {server['ssh_user']}@{server['ssh_hostname']} \
         -i {path_to_config_file(server['ssh_key'])} \
-        -p {server['ssh_port']} {run}"
+        -p {server['ssh_port']} \
+        -o StrictHostKeyChecking=no \
+        {run}"
     while command.find("  ") != -1:
         command = command.replace("  ", " ")
     print(command)
