0d4ab56de05bd9403d24712580b12700de954c08
Tom Parker-Shemilt <palfrey@tevp.net>
2023-05-20 14:56:08 +0100

    Add args to test cockroach

--- integration_test/main.py
+++ integration_test/main.py
@@ -4,4 +4,10 @@ from paracrine.runner import run
 from paracrine.services import cockroachdb, pleroma
 
 if __name__ == "__main__":
-    run(sys.argv[1], [pleroma, cockroachdb])
+    run(
+        sys.argv[1],
+        [
+            pleroma,
+            (cockroachdb, {"HTTP_PORT": 9080}),
+        ],
+    )
