e8ad5ec27a4cc30165b43511eb6c7861d1186629
Tom Parker-Shemilt <palfrey@tevp.net>
2024-07-22 20:20:59 +0100

    Add simple cockroach version test

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -19,3 +19,20 @@ def test_deps_ordering():
         ("paracrine.services.cockroachdb.init", {"versions": {}}),
         "paracrine.services.cockroachdb",
     ]
+
+
+def test_cockroach_versions():
+    modules: Modules = [cockroachdb]
+    set_data({"templates": "", "inventory": {"servers": [{"name": "foo"}]}})
+    modules = generate_dependencies(modules)
+
+    assert maketransmit(modules) == [
+        "paracrine.runners.core",
+        ("paracrine.services.cockroachdb.certs", {"versions": {"foo": "23.1.1"}}),
+        "paracrine.services.wireguard.bootstrap",
+        "paracrine.services.wireguard.core",
+        "paracrine.services.wireguard",
+        ("paracrine.services.cockroachdb.node", {"versions": {"foo": "23.1.1"}}),
+        ("paracrine.services.cockroachdb.init", {"versions": {"foo": "23.1.1"}}),
+        "paracrine.services.cockroachdb",
+    ]
