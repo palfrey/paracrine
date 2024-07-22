88ef80fc784af83db84740222c77b7169998dd5e
Tom Parker-Shemilt <palfrey@tevp.net>
2024-07-22 20:19:05 +0100

    Fix dependency tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,19 +1,21 @@
 from paracrine.deps import Modules, maketransmit
+from paracrine.helpers.config import set_data
 from paracrine.runner import generate_dependencies
 from paracrine.services import cockroachdb, wireguard
 
 
 def test_deps_ordering():
     modules: Modules = [wireguard, cockroachdb]
+    set_data({"templates": "", "inventory": {"servers": []}})
     modules = generate_dependencies(modules)
 
     assert maketransmit(modules) == [
         "paracrine.runners.core",
-        "paracrine.services.cockroachdb.certs",
+        ("paracrine.services.cockroachdb.certs", {"versions": {}}),
         "paracrine.services.wireguard.bootstrap",
         "paracrine.services.wireguard.core",
         "paracrine.services.wireguard",
-        ("paracrine.services.cockroachdb.node", {}),
-        ("paracrine.services.cockroachdb.init", {}),
+        ("paracrine.services.cockroachdb.node", {"versions": {}}),
+        ("paracrine.services.cockroachdb.init", {"versions": {}}),
         "paracrine.services.cockroachdb",
     ]
