11b4e1733e7dd9bfa3e343a605f2c779c6cbfe8a
Tom Parker-Shemilt <palfrey@tevp.net>
2026-01-29 21:21:39 +0000

    Downgrade setup to 3.13

--- paracrine/commands/setup.py
+++ paracrine/commands/setup.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
         """apt-get update &&
         apt-get install -y curl sudo &&
         (curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.9.18/uv-installer.sh | sh) &&
-        ~/.local/bin/uv python install 3.14 &&
-        ln -s ~/.local/bin/python3.14 /usr/local/bin/python3""",
+        ~/.local/bin/uv python install 3.13 &&
+        rm -f /usr/local/bin/python3 &&
+        ln -s ~/.local/bin/python3.13 /usr/local/bin/python3""",
         as_root=True,
     )
