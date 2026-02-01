6fcc5e5db805a44a8fd41ce3215807c9b818d929
Tom Parker-Shemilt <palfrey@tevp.net>
2026-02-01 20:51:02 +0000

    Remove old python symlink

--- paracrine/commands/setup.py
+++ paracrine/commands/setup.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
         apt-get install -y curl sudo &&
         (curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.9.28/uv-installer.sh | sh) &&
         ~/.local/bin/uv python install 3.13 &&
-        ln -s ~/.local/bin/python3.13 /usr/bin/python3""",
+        rm -f /usr/local/bin/python3 &&
+        ln -s ~/.local/bin/python3.13 /usr/local/bin/python3""",
         as_root=True,
     )
