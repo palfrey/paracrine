619c14aafc2b872bdfbfba4331bb8a5fbf477005
Tom Parker-Shemilt <palfrey@tevp.net>
2026-05-30 22:33:25 +0100

    If no kernel upgrade needed, don't do it

--- paracrine/services/kernel/do_upgrade.py
+++ paracrine/services/kernel/do_upgrade.py
@@ -1,6 +1,6 @@
 import json
 
-from paracrine.helpers.config import get_config_file
+from paracrine.helpers.config import get_config_file, host
 from paracrine.helpers.debian import apt_install
 
 from .common import get_all_kernel_versions
@@ -18,6 +18,8 @@ def dependencies():
 def run():
     upgrade_info_raw = get_config_file(f"configs/{KERNEL_UPGRADE_KEY}")
     upgrade_info: UpgradeInfo = json.loads(upgrade_info_raw)
+    if upgrade_info["hostname"] != host()["name"]:  # which includes the None case
+        return
     all_versions = get_all_kernel_versions()
     for key, value in all_versions.items():
         if key == upgrade_info["version"]:
